30ffd20c9ce0f57491068c7cbd8dcbc13e57bdac
apkmasondev <apkmasondev@github.com>
2026-06-27 21:01:48 +0200

    fix(ts): use fragments instead of booleans for EffectComposer

--- src/components/Scene3D/Scene3D.tsx
+++ src/components/Scene3D/Scene3D.tsx
@@ -256,10 +256,9 @@ const SceneContent = ({ isMobile, disableEffects }: { isMobile: boolean, disable
           />
         )}
         {!isMobile && (
-          // @ts-ignore: @react-three/postprocessing types are broken for conditional children
           <EffectComposer multisampling={4}>
-            {dofEnabled && !disableEffects && <DepthOfField key="dof" target={dofTarget} focalLength={0.05} bokehScale={8} height={700} />}
-            {!disableEffects && <N8AO key="n8ao" aoRadius={0.5} intensity={2.0} distanceFalloff={0.5} quality="medium" halfRes />}
+            {dofEnabled && !disableEffects ? <DepthOfField key="dof" target={dofTarget} focalLength={0.05} bokehScale={8} height={700} /> : <></>}
+            {!disableEffects ? <N8AO key="n8ao" aoRadius={0.5} intensity={2.0} distanceFalloff={0.5} quality="medium" halfRes /> : <></>}
             <Bloom key="bloom" luminanceThreshold={0.5} mipmapBlur intensity={1.5} />
             <Vignette key="vig" eskil={false} offset={0.1} darkness={0.9} />
             <ChromaticAberration key="ca" offset={new Vector2(0.0005, 0.0005)} radialModulation={false} modulationOffset={0} />
